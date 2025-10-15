685e4e6e7db30477cb998b2138d29403df18204b
bogdash <ms.anastasia.bogdanova@gmail.com>
2025-10-15 10:51:45 +0300

    Добавлен текст

--- app/src/main/java/com/bogdash/stepikcourse/MainActivity.kt
+++ app/src/main/java/com/bogdash/stepikcourse/MainActivity.kt
@@ -22,8 +22,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bogdash.stepikcourse.ui.theme.InstagramProfileCard
 import com.bogdash.stepikcourse.ui.theme.StepikCourseTheme
 
@@ -43,4 +51,22 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun TestText() {
+    Text(
+        buildAnnotatedString {
+            withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                append("Hello")
+            }
+            withStyle(SpanStyle(textDecoration = TextDecoration.Underline)) {
+                append(" ")
+            }
+            withStyle(SpanStyle(fontSize = 30.sp, textDecoration = TextDecoration.LineThrough)) {
+                append("World!")
+
+            }
+        }
+    )
 }
