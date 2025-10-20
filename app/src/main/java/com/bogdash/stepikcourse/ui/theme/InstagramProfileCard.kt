4744c96f8ebc4b86b6154729b78a0cd355dd753a
bogdash <ms.anastasia.bogdanova@gmail.com>
2025-10-20 11:02:40 +0300

    Добавление кнопки

--- app/src/main/java/com/bogdash/stepikcourse/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/bogdash/stepikcourse/ui/theme/InstagramProfileCard.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -42,35 +44,58 @@ fun InstagramProfileCard() {
         ),
         border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier.padding(16.dp)
         ) {
-            Image(
+            Row(
                 modifier = Modifier
-                    .clip(CircleShape)
-                    .background(Color.White)
-                    .padding(8.dp)
-                    .size(60.dp),
-                painter = painterResource(R.drawable.ic_instagram),
-                contentDescription = null,
-                contentScale = ContentScale.FillHeight
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .padding(8.dp)
+                        .size(60.dp),
+                    painter = painterResource(R.drawable.ic_instagram),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillHeight
+                )
+                UserStatistics(
+                    title = "Posts",
+                    value = "6,950"
+                )
+                UserStatistics(
+                    title = "Followers",
+                    value = "436M"
+                )
+                UserStatistics(
+                    title = "Following",
+                    value = "76"
+                )
+            }
+            Text(
+                text = "Instagram",
+                fontSize = 32.sp,
+                fontFamily = FontFamily.Cursive
             )
-            UserStatistics(
-                title = "Posts",
-                value = "6,950"
+            Text(
+                text = "#YoursToMake",
+                fontSize = 14.sp
             )
-            UserStatistics(
-                title = "Followers",
-                value = "436M"
+            Text(
+                text = "www.facebook.com/emotional_health",
+                fontSize = 14.sp
             )
-            UserStatistics(
-                title = "Following",
-                value = "76"
-            )
+            Button(
+                onClick = {}
+            ) {
+                Text(
+                    text = "Follow"
+                )
+            }
         }
     }
 }
