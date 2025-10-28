687398d8db863bdad34a4016d03d08af8ca35acd
bogdash <ms.anastasia.bogdanova@gmail.com>
2025-10-28 10:56:31 +0300

    3.2. Изучение Scaffold, Drawer, NavigationBar

--- app/src/main/java/com/bogdash/stepikcourse/MainActivity.kt
+++ app/src/main/java/com/bogdash/stepikcourse/MainActivity.kt
@@ -4,16 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -27,69 +40,100 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-private fun ColumnScope.TestText(count: Int, text: String) {
-    repeat(count) {
-        Text(
-            modifier = Modifier.weight(1f),
-            text = text
-        )
-    }
-}
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Test() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                NavigationDrawerItem(
+                    label = {
+                        Text(" Text 1")
+                    },
+                    selected = true,
+                    onClick = {},
+                    icon = {
+                        Icon(Icons.Filled.Call, null)
+                    }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                NavigationDrawerItem(
+                    label = {
+                        Text(" Text 2")
+                    },
+                    selected = false,
+                    onClick = {},
+                    icon = {
+                        Icon(Icons.Filled.Build, null)
+                    }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                NavigationDrawerItem(
+                    label = {
+                        Text(" Text 3")
+                    },
+                    selected = false,
+                    onClick = {},
+                    icon = {
+                        Icon(Icons.Filled.AccountCircle, null)
+                    }
+                )
+            }
+        }
     ) {
-        Example3()
-    }
-}
-
-@Composable
-private fun Example1() {
-    OutlinedButton(
-        onClick = {}
-    ) {
-        Text(text = "Hello world")
-    }
-}
-
-@Composable
-private fun Example2() {
-    TextField(
-        value = "",
-        onValueChange = {},
-        label = { Text(text = "Label") }
-    )
-}
-
-@Composable
-private fun Example3() {
-    AlertDialog(
-        onDismissRequest = {},
-        confirmButton = {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text( text = "TopAppBarTitle" )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {}) {
+                            Icon(Icons.Filled.Menu, contentDescription = null)
+                        }
+                    }
+                )
+            },
+            bottomBar = {
+                NavigationBar {
+                    NavigationBarItem(
+                        selected = true,
+                        onClick = {},
+                        icon = {
+                            Icon(Icons.Filled.Favorite, contentDescription = null)
+                        },
+                        label = {
+                            Text("Favorite")
+                        }
+                    )
+                    NavigationBarItem(
+                        selected = false,
+                        onClick = {},
+                        icon = {
+                            Icon(Icons.Outlined.Edit, contentDescription = null)
+                        },
+                        label = {
+                            Text("Edit")
+                        }
+                    )
+                    NavigationBarItem(
+                        selected = false,
+                        onClick = {},
+                        icon = {
+                            Icon(Icons.Outlined.Delete, contentDescription = null)
+                        },
+                        label = {
+                            Text("Delete")
+                        }
+                    )
+                }
+            }
+        ) {
             Text(
-                modifier = Modifier.padding(8.dp),
-                text = "Yes"
-            )
-        },
-        title = {
-            Text(
-                text = "Are you sure?"
-            )
-        },
-        text = {
-            Text(
-                text = "Do you want to delete this file?"
-            )
-        },
-        dismissButton = {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text = "No"
+                modifier = Modifier.padding(it),
+                text = "This is scaffold content"
             )
         }
-    )
+    }
+
 }
