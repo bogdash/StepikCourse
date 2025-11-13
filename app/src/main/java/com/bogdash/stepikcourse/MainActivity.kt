3b27eaa6f3d2b24ffad975fe1a18927ea1f58d84
bogdash <ms.anastasia.bogdanova@gmail.com>
2025-11-13 10:54:47 +0300

    4.6 Изучение LazyColumn

--- app/src/main/java/com/bogdash/stepikcourse/MainActivity.kt
+++ app/src/main/java/com/bogdash/stepikcourse/MainActivity.kt
@@ -4,22 +4,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.lifecycle.ViewModelProvider
+import com.bogdash.stepikcourse.ui.theme.InstagramProfileCard
 import com.bogdash.stepikcourse.ui.theme.StepikCourseTheme
-import com.bogdash.stepikcourse.ui.theme.VkNewsMainScreen
 
 class MainActivity : ComponentActivity() {
-    private val viewModel by viewModels<VkNewsViewModel>()
+//    private val viewModel by viewModels<VkNewsViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val viewModel = ViewModelProvider(this)[InstagramViewModel::class.java]
+        val viewModel = ViewModelProvider(this)[InstagramViewModel::class.java]
         enableEdgeToEdge()
         setContent {
-            StepikCourseTheme {
-                VkNewsMainScreen(
+            Test(viewModel = viewModel)
+//            StepikCourseTheme {
+//                VkNewsMainScreen(
+//                    viewModel = viewModel
+//                )
+//            }
+        }
+    }
+}
+
+@Composable
+private fun Test(viewModel: InstagramViewModel) {
+    StepikCourseTheme {
+        LazyColumn {
+            item {
+                Text(text = "Title")
+            }
+            items(10) {
+                InstagramProfileCard(
+                    viewModel = viewModel
+                )
+            }
+            item {
+                Image(painter = painterResource(id = R.drawable.ic_instagram), contentDescription = null)
+            }
+            items(500) {
+                InstagramProfileCard(
                     viewModel = viewModel
                 )
             }
         }
+
     }
 }
